26ae051ec35f73eda4f031362be203cd774667b3
NGUYEN LE THANH <forenamethanh@gmail.com>
2022-12-06 10:54:42 +0700

    upload and download file pdf FIX ERROR THREAD

---
+++ TheStarBuck/src/main/java/vn/edu/hcmuaf/fit/laptrinhweb/controller/web/payment/DownloadInvoice.java
@@ -0,0 +1,83 @@
+package vn.edu.hcmuaf.fit.laptrinhweb.controller.web.payment;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import vn.edu.hcmuaf.fit.laptrinhweb.model.Account;
+import vn.edu.hcmuaf.fit.laptrinhweb.model.Cart;
+import vn.edu.hcmuaf.fit.laptrinhweb.model.Orders;
+import vn.edu.hcmuaf.fit.laptrinhweb.model.Product;
+import vn.edu.hcmuaf.fit.laptrinhweb.service.impl.OrderService;
+
+import java.io.IOException;
+
+@WebServlet(name = "CheckoutPayment_Servlet", value = "/payment-checkout")
+public class DownloadInvoice extends HttpServlet {
+    private OrderService orderService;
+    private GenderPdf genderPdf;
+
+    public DownloadInvoice() {
+        orderService = OrderService.getInstance();
+        genderPdf = GenderPdf.instance();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        request.getRequestDispatcher("/views/web/payment.jsp").forward(request, response);
+        doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//get cart from session
+        HttpSession session = request.getSession();
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        Cart cart = (Cart) session.getAttribute("cart");
+        Account account = (Account) session.getAttribute("account");
+        Orders orders = new Orders();
+        String name = request.getParameter("name");
+        String phone = request.getParameter("phone");
+        String email = request.getParameter("email");
+        String note = "name: " + name + ", phone number: " + phone + ", email: " + email;
+        String addresses = request.getParameter("addresses");
+        String addressDetail = request.getParameter("addressDetail");
+        String payment = request.getParameter("payment");
+        String address = addresses + "\nDetail: " + addressDetail;
+        orders.setIdAccount(account.getId());
+        orders.setSubTotal(cart.getSubTotalPrice());
+        orders.setGrandTotal(cart.getTotalPrice());
+        orders.setNote(note);
+        orders.setAddress(address);
+        orders.setIdSession("session");
+        orders.setToken("token");
+        orders.setStatus("status");
+        orders.setItemDiscount(1);
+        orders.setTax(1);
+        orders.setShipping(1);
+        orders.setId("");
+        orders.setPromo("");
+        for(Product product:cart.getProductList()){
+            orders.getProductList().put(product.getId(),product);
+        }
+
+
+        boolean checkFlag = orderService.createOrder(account, cart, orders);
+        System.out.println("------ " + checkFlag);
+        if (checkFlag) {
+            //generate PDF:
+            genderPdf.generatePDF(account,orders,request);
+            Orders orders1 = orderService.getItemByIdAc(account.getId());
+            session.removeAttribute("cart");
+          //  session.setAttribute("order", orders1);
+          //  session.setAttribute("productSold", cart.getProductList().stream().collect(toCollection(ArrayList::new)));
+            request.getRequestDispatcher("/views/web/order.jsp").forward(request, response);
+        } else {
+            response.sendRedirect(request.getContextPath() + "/payment");
+        }
+    }
+
+}
---
+++ TheStarBuck/src/main/webapp/views/web/selectRolesPage.jsp
@@ -0,0 +1,49 @@
+<%@ page import="vn.edu.hcmuaf.fit.laptrinhweb.controller.web.Asset" %>
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@include file="/common/taglib.jsp"%>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Select</title>
+    <style>
+        .image-management {
+    background-image: url('https://scx1.b-cdn.net/csz/news/800/2017/theoreticala.jpg');
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: 100% 100%;
+    color: #FFFFFF;
+}
+        .image-home {
+            background-image: url('https://scx1.b-cdn.net/csz/news/800/2017/theoreticala.jpg');
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: 100% 100%;
+            color: #FFFFFF;
+        }
+    </style>
+</head>
+
+<body>
+<div>
+    <div>
+        <div class="image-management">
+        </div>
+       <div>MANAGEMENT</div>
+    </div>
+    <div>
+        <div class="image-home"></div>
+        <div>HOME</div>
+    </div>
+
+</div>
+<script>
+
+</script>
+</body>
+
+</html>
