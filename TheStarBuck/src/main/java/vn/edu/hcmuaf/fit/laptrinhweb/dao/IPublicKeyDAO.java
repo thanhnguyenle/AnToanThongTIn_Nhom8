82e21140fd108d2a5836608fac227ad0b6f21648
NGUYEN LE THANH <forenamethanh@gmail.com>
2022-12-08 00:02:33 +0700

    add database publickey and build DAO for publickey service

---
+++ TheStarBuck/src/main/java/vn/edu/hcmuaf/fit/laptrinhweb/dao/IPublicKeyDAO.java
@@ -0,0 +1,22 @@
+package vn.edu.hcmuaf.fit.laptrinhweb.dao;
+
+import vn.edu.hcmuaf.fit.laptrinhweb.model.Account;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+public interface IPublicKeyDAO extends IGenericDAO<Account> {
+    List<Account> findAll();
+
+    Long save(Account account);
+
+    int getAmountItem();
+
+    Account getItem(String id);
+
+    Account getAccountByEmail(String email);
+
+    boolean verifyAccount(String email);
+
+    public String mdbPassword(String password) throws NoSuchAlgorithmException;
+}
---
+++ TheStarBuck/src/main/java/vn/edu/hcmuaf/fit/laptrinhweb/mapper/impl/PublicKeyMapper.java
@@ -0,0 +1,40 @@
+package vn.edu.hcmuaf.fit.laptrinhweb.mapper.impl;
+
+import vn.edu.hcmuaf.fit.laptrinhweb.mapper.IRowMapper;
+import vn.edu.hcmuaf.fit.laptrinhweb.model.Orders;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PublicKeyMapper implements IRowMapper<Orders> {
+    @Override
+    public Orders mapRow(ResultSet resultSet) {
+        try {
+            Orders orders = new Orders();
+            orders.setId(resultSet.getString("id_od"));
+            orders.setIdAccount(resultSet.getString("od_acId"));
+            orders.setIdSession(resultSet.getString("od_sessionId"));
+            orders.setName(resultSet.getString("od_name"));
+            orders.setPhone(resultSet.getString("od_phone"));
+            orders.setEmail(resultSet.getString("od_email"));
+            orders.setToken(resultSet.getString("od_token"));
+            orders.setStatus(resultSet.getString("od_status"));
+            orders.setAddress(resultSet.getString("od_address"));
+            orders.setSubTotal(resultSet.getDouble("od_subTotal"));
+            orders.setItemDiscount(resultSet.getDouble("od_itemDiscount"));
+            orders.setTax(resultSet.getDouble("od_tax"));
+            orders.setShipping(resultSet.getDouble("od_shipping"));
+            orders.setGrandTotal(resultSet.getDouble("od_grandTotal"));
+            orders.setPromo(resultSet.getString("od_promo"));
+            orders.setNote(resultSet.getString("od_note"));
+            orders.setCreatedDate(resultSet.getDate("createdDate"));
+            orders.setModifiedDate(resultSet.getDate("modifiedDate"));
+            orders.setCreatedBy(resultSet.getString("createdBy"));
+            orders.setModifiedBy(resultSet.getString("modifiedBy"));
+            return orders;
+        } catch (SQLException e) {
+            return null;
+        }
+
+    }
+}
